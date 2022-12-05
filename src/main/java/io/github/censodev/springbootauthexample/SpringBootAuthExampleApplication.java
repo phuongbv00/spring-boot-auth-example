a1c76d0c60ff744189943d9278f1a767f160a4fe
Phuong Bui <phuong.bui.viet@ncs.com.sg>
2022-12-05 15:50:06 +0700

    starter

---
+++ src/main/java/io/github/censodev/springbootauthexample/SpringBootAuthExampleApplication.java
@@ -0,0 +1,13 @@
+package io.github.censodev.springbootauthexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAuthExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAuthExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/censodev/springbootauthexample/SpringBootAuthExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.censodev.springbootauthexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAuthExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
