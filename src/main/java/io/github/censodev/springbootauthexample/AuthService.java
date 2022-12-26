b13fe7abaaddbc3585c9d6aa4988d9eb25083ef2
Phuong Bui <51000027+censodev@users.noreply.github.com>
2022-12-26 16:28:36 +0700

    Update AuthService.java

--- src/main/java/io/github/censodev/springbootauthexample/AuthService.java
+++ src/main/java/io/github/censodev/springbootauthexample/AuthService.java
@@ -3,11 +3,9 @@ package io.github.censodev.springbootauthexample;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.stereotype.Service;
 
 import java.util.Collection;
 
-@Service
 public interface AuthService {
 
     @Getter
